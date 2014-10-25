23005f45a12cbec18d7c824b50ccfa41792c9896
waqasmoazzam <waqasmoazzam87@gmail.com>
2014-10-25 23:47:02 +0200

    error page and a div structure for an article respresentation

--- node.js/server.js
+++ node.js/server.js
@@ -1,16 +1,28 @@
 // reference the http module so we can create a webserver
 var http = require("http");
 var fs = require("fs");
+var htmlData;
+var pagePath="../html/404.html";
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }
 
 // create a server
-http.createServer(function(req, res) {
-    // on every request, we'll output 'Hello world'
-    response.setHeader("Content-Type", "text/html");
-    
-}).listen(3000);
+	http.createServer(function(req, res) {
+	    // on every request, we'll output 'Hello world'
+	    if(req.url=='/index.html' || req.url=='/') {
+			pagePath = "../html/index.html";
+		}
+
+		fs.readFile(pagePath, function (err, htmlData) {
+		    	if (err) {
+		    		res.statusCode = 404;
+		        	throw err; 
+		    	} else {
+		    		res.statusCode = 200;
+		    	}
+				res.setHeader("Content-Type", "text/html");
+	    		res.write(htmlData);
+	    		res.end();
+			});
+
+	}).listen(3000);
 
